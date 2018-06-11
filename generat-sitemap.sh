8a93acf67d0325769b66afb60c3c558397522d9a
Ravikant Kumar <ravikant.kumar@squareyards.co.in>
2018-06-11 17:07:50 +0530

    updated

--- generat-sitemap.sh
+++ generat-sitemap.sh
@@ -15,10 +15,6 @@ cd $pythonDir
 rm -f output/sitemap.xml ## Removing before generating new
 sudo python sitemap_gen.py --config=ca-config.xml --testing &> /dev/null
 
-##logrotate -- custom log
-/usr/sbin/logrotate --force /app/web/www.squareyards.ca/sitemap-ca/logrotatefile ## After generating sitemap log file is rotated.
-echo "custom log file rotated"
-##
 
 newdevlopment() {
 	query="select concat('<url><loc>','https://www.squareyards.ca/',
@@ -41,8 +37,46 @@ from ca_project prj;"
 
 }
 
+sale_sitemap() {
+	query="select concat('<url><loc>','https://www.squareyards.ca/properties/',wp_wpl_properties.id,'-',Lower(wp_wpl_properties.alias),'/sale', '</loc><changefreq>daily</changefreq><priority>0.8</priority></url>')as link
+from wp_wpl_properties where deleted = 0 and confirmed = 1 and finalized = 1 and expired = 0 and listing = 14;"
+	touch $sitemapDir/sitemap-sale-temp.xml
+        cat $sitemapDir/default.xml >> $sitemapDir/sitemap-sale-temp.xml
+        mysql -h localhost -u sitemap -pw3lc0m3 ca_mls --skip-column-names -e "${query}" >> $sitemapDir/sitemap-sale-temp.xml
+        if [ $? != 0 ];then
+                echo "DB connection Problem"
+                rm -f $sitemapDir/sitemap-sale-temp.xml
+                exit
+        fi
+        echo "</urlset>" >> $sitemapDir/sitemap-sale-temp.xml
+        echo "REPLACING & with &amp; in sitemap-sale-temp.xml"
+        sed -i s/"&"/"&amp;"/g $sitemapDir/sitemap-sale-temp.xml
+        mv $sitemapDir/sitemap-sale-temp.xml $sitemapDir/sitemap-sale.xml
+        echo "sitemap of sale created"
+}
+
+lease_sitemap() {
+	query="select concat('<url><loc>','https://www.squareyards.ca/properties/',wp_wpl_properties.id,'-',Lower(wp_wpl_properties.alias),'/lease',
+'</loc><changefreq>daily</changefreq><priority>0.8</priority></url>')as link
+from wp_wpl_properties where deleted = 0 and confirmed = 1 and finalized = 1 and expired = 0 and listing in (13,15);"
+        touch $sitemapDir/sitemap-lease-temp.xml
+        cat $sitemapDir/default.xml >> $sitemapDir/sitemap-lease-temp.xml
+        mysql -h localhost -u sitemap -pw3lc0m3 ca_mls --skip-column-names -e "${query}" >> $sitemapDir/sitemap-lease-temp.xml
+        if [ $? != 0 ];then
+                echo "DB connection Problem"
+                rm -f $sitemapDir/sitemap-lease-temp.xml
+                exit
+        fi
+        echo "</urlset>" >> $sitemapDir/sitemap-lease-temp.xml
+        echo "REPLACING & with &amp; in sitemap-lease-temp.xml"
+        sed -i s/"&"/"&amp;"/g $sitemapDir/sitemap-lease-temp.xml
+        mv $sitemapDir/sitemap-lease-temp.xml $sitemapDir/sitemap-lease.xml
+        echo "sitemap of lease created"
+}
+
 ## This section for new-devlopment sitemap. sitemap.xml is generated form accesslog file.
 locationBased(){
+
 	## Below code for random url generate -- location based search.
 	n=0
 	u=0
@@ -72,18 +106,44 @@ locationBased(){
 
 
 cd $sitemapDir
+
 ## For New Devlopment
 if [ $1 == 'newdev' ];then
 newdevlopment
 fi
+
+## For sale sitemap
+if [ $1 == 'sale' ];then
+sale_sitemap
+fi
+
+## for lease sitemap
+if [ $1 == 'lease' ];then
+lease_sitemap
+fi
+
 ## For Location Based
 if [ $1 == 'location' ];then
 locationBased
+
+	##logrotate -- custom log
+	/usr/sbin/logrotate --force /app/web/www.squareyards.ca/sitemap-ca/logrotatefile ## After generating sitemap log file is rotated.
+	echo "custom log file rotated"
+	##
+
 fi
+
 ## For All
 if [ $1 == "all" ];then
 locationBased
 newdevlopment
+sale_sitemap
+lease_sitemap
+	##logrotate -- custom log
+	/usr/sbin/logrotate --force /app/web/www.squareyards.ca/sitemap-ca/logrotatefile ## After generating sitemap log file is rotated.
+	echo "custom log file rotated"
+	##
+
 fi
 
 ## Copy .xml file in project dir.
